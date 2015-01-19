d14418599f5b0318c3a6a31ad322c6bb96f9192b
mifl <mifl.pc@gmail.com>
2015-01-19 17:05:37 +0100

    msm8960: move Audio UCM files to device repos
    Change-Id: I383bfe7f60237b88068c86a6e87c1ff8b5046674

--- msm8960.mk
+++ msm8960.mk
@@ -24,13 +24,6 @@ DEVICE_PACKAGE_OVERLAYS := device/pantech/msm8960-common/overlay
 # Audio policy
 PRODUCT_COPY_FILES += device/pantech/msm8960-common/prebuilt/system/etc/audio_policy.conf:system/etc/audio_policy.conf
 
-# Audio UCM files
-PRODUCT_COPY_FILES += \
-    device/pantech/msm8960-common/snd_soc_msm/snd_soc_msm:system/etc/snd_soc_msm/snd_soc_msm \
-    device/pantech/msm8960-common/snd_soc_msm/snd_soc_msm_2x:system/etc/snd_soc_msm/snd_soc_msm_2x \
-    device/pantech/msm8960-common/snd_soc_msm/snd_soc_msm_2x_Fusion3:system/etc/snd_soc_msm/snd_soc_msm_2x_Fusion3 \
-    device/pantech/msm8960-common/snd_soc_msm/snd_soc_msm_Sitar:system/etc/snd_soc_msm/snd_soc_msm_Sitar \
-
 # Bluetooth configuration files
 #PRODUCT_COPY_FILES += system/bluetooth/data/main.le.conf:system/etc/bluetooth/main.conf
 
@@ -51,11 +44,7 @@ PRODUCT_COPY_FILES += device/pantech/msm8960-common/prebuilt/system/etc/thermald
 PRODUCT_PACKAGES += \
     alsa.msm8960 \
     audio.r_submix.default \
-    libaudio-resampler \
-    snd_soc_msm \
-    snd_soc_msm_2x \
-    snd_soc_msm_2x_Fusion3 \
-    snd_soc_msm_Sitar
+    libaudio-resampler
 
 # Battery
 PRODUCT_PACKAGES += \
