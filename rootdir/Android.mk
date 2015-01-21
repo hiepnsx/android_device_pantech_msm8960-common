2102c8e107a4b4a4a5f0ece1f5625b710f3af581
mifl <mifl.pc@gmail.com>
2015-01-21 12:38:12 +0100

    msm8960: Move init.target.rc declaration to device repositories
    This file must be in device repos because have dependencies with fstab.qcom and init.qcom.rc
    
    Change-Id: Ia62ed8cbdb91db004f1fb42e25d73e70f8798b15

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -3,13 +3,13 @@ include $(CLEAR_VARS)
 
 # files that live under device/pantech/msm8960-common/rootdir/etc/
 
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.target.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/$(LOCAL_MODULE)
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
+#include $(CLEAR_VARS)
+#LOCAL_MODULE       := init.target.rc
+#LOCAL_MODULE_TAGS  := optional
+#LOCAL_MODULE_CLASS := ETC
+#LOCAL_SRC_FILES    := etc/$(LOCAL_MODULE)
+#LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+#include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.ril.sh
